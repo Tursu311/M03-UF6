915618f64dd7216b1e70b1d37595661530970ecf
Tursu <[nil.masso.caba@gmail.com]>
2023-05-04 10:06:00 +0200

    .

--- src/DAO.java
+++ src/DAO.java
@@ -5,7 +5,7 @@
 import java.sql.*;
 import java.util.Scanner;
 
-public class ProgramaPrincipal {
+public class DAO {
     public static Connection con;
 
     public static void main(String[] args) throws SQLException {
