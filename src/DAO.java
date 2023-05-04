cba7b368fe69b4030985f92923f70b8237ed1caa
Tursu <[nil.masso.caba@gmail.com]>
2023-05-04 09:59:46 +0200

    .

--- src/DAO.java
+++ src/DAO.java
@@ -13,9 +13,9 @@ public class ProgramaPrincipal {
         Scanner sc = new Scanner(System.in);
         int opcio = 0;
         do {
-            System.out.println("1. Mostrar percentatge de dones i homes per comunitat autonoma");
+            System.out.println("1. Mostrar percentatge de dones i homes per candidatura");
             System.out.println("2. Mostrar percentatge de dones i homes per provincia");
-            System.out.println("4. Mostrar percentatge de dones i homes per candidatura");
+            System.out.println("4. Mostrar percentatge de dones i homes per comunitat autonoma");
             System.out.println("5. Sortir");
             opcio = sc.nextInt();
             switch (opcio) {
@@ -36,7 +36,50 @@ public class ProgramaPrincipal {
         con.close();
     }
 
-    public static void donesHomesComunitatAutonoma() throws SQLException {
+    //Tenim una taula candidats on hi ha la id de la persona( que aquest id esta a la taula persones, on esta el genere), i la id de la candidatura.
+    //fem un metode que ens mostri el percentatge de dones i homes per candidatura
+    public static void donesHomesCandidatura() throws SQLException {
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM candidatures");
+        while (rs.next()) {
+            String nomCandidatura = rs.getString("nom");
+            int idCandidatura = rs.getInt("id");
+            Statement st2 = con.createStatement();
+            ResultSet rs2 = st2.executeQuery("SELECT COUNT(*) FROM candidats WHERE id_candidatura = " + idCandidatura);
+            rs2.next();
+            int totalPersones = rs2.getInt(1);
+            Statement st3 = con.createStatement();
+            ResultSet rs3 = st3.executeQuery("SELECT COUNT(*) FROM candidats WHERE id_candidatura = " + idCandidatura + " AND sexe = 'Dona'");
+            rs3.next();
+            int dones = rs3.getInt(1);
+            Statement st4 = con.createStatement();
+            ResultSet rs4 = st4.executeQuery("SELECT COUNT(*) FROM candidats WHERE id_candidatura = " + idCandidatura + " AND sexe = 'Home'");
+            rs4.next();
+            int homes = rs4.getInt(1);
+            System.out.println("Candidatura: " + nomCandidatura);
+            System.out.println("Dones: " + dones + " Homes: " + homes);
+            System.out.println("Percentatge dones: " + (dones * 100 / totalPersones) + "%");
+            System.out.println("Percentatge homes: " + (homes * 100 / totalPersones) + "%");
+            System.out.println();
+        }
+    }
+
+    //fem un metode que ens mostri el percentatge de dones i homes per provincia
+    public static void donesHomesProvincia() throws SQLException {
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM provincies");
+        while (rs.next()) {
+            String nomProvincia = rs.getString("nom");
+            int idProvincia = rs.getInt("id");
+            Statement st2 = con.createStatement();
+            ResultSet rs2 = st2.executeQuery("SELECT COUNT(*) FROM persones WHERE id_provincia = " + idProvincia);
+            rs2.next();
+            int totalPersones = rs2.getInt(1);
+            Statement st3
+
+
+
+    
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery("SELECT * FROM comunitats_autonomes");
         while (rs.next()) {
@@ -61,4 +104,3 @@ public class ProgramaPrincipal {
             System.out.println();
         }
     }
-    aa
