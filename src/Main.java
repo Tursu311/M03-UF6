b893d44929ca6e501b8216ae42c99d0c1a47ff34
Alejandro Vázquez Carrión <123106711+ElTinii@users.noreply.github.com>
2023-05-09 10:35:08 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            CandidatureDAO.donesHomesCandidatura();
-            AddCandidat.main(args);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws SQLException {
+        menu(args);
     }
+    public static void menu(String[] args) throws SQLException {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("1. Afegir candidat");
+        System.out.println("2. Mostrar percentatge d'homes i dones que hi ha a una candidatura");
+        System.out.println("3. Modificar");
+        System.out.println("4. Sortir");
+        System.out.print("Escull una opció: ");
+        int opcio = scan.nextInt();
+
+        switch(opcio){
+            case 1: AMDCandidat.main(args);
+            break;
+            case 2: CandidatureDAO.donesHomesCandidatura();
+            break;
+            case 3: break;
+            case 4: System.exit(0);
+        }
+
+
+    }
+
 }
