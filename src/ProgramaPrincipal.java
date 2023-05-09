f6ae30bbde7d236a75359fea696420dfa16d8ea6
Alejandro Vázquez Carrión <123106711+ElTinii@users.noreply.github.com>
2023-05-09 09:19:14 +0200

    .

--- src/ProgramaPrincipal.java
+++ src/ProgramaPrincipal.java
@@ -5,49 +5,16 @@ import java.sql.SQLException;
 public class ProgramaPrincipal {
     public static Connection con;
 
-    public static void main(String[] args) throws SQLException {
+    public void main(String[] args) throws SQLException {
         con = DriverManager.getConnection("jdbc:mysql://192.168.56.103/programacio?serverTimezone=UTC", "perepi", "pastanaga");
         ImportarComunitatsAutonomes();
-        System.out.println("Comunitats Autonomes importades");
-        ImportProvincies();
-        System.out.println("Provincies importades");
-        ImportMunicipis();
-        System.out.println("Municipis importats");
-        ImportarCandidatures();
-        System.out.println("Candidatures importades");
-        ImportPersonas();
-        System.out.println("Personas importades");
-        ImportarCandidats();
-        System.out.println("Candidats importats");
         con.close();
     }
 
     // Importación de los datos de la tabla Comunitats Autonomes
-    public static void ImportarComunitatsAutonomes() {
+    public void ImportarComunitatsAutonomes() {
 
-        Leer.comunitatsAutonomes(con);
+        CandidatDAO.countByCandidatura();
     }
-    // Importación de los datos de la tabla Provincies
-    public static void ImportProvincies() {
 
-        Leer.provincies(con);
-    }
-    // Importación de los datos de lal tabla Municipis
-    public static void ImportMunicipis() {
-
-        Leer.municipis(con);
-    }
-    public static void ImportarCandidatures(){
-
-        Leer.candidatures(con);
-    }
-    // Importación de los datos de la tabla Personas
-    public static void ImportPersonas() {
-
-        Leer.personas(con);
-    }
-    public static void ImportarCandidats(){
-
-        Leer.candidats(con);
-    }
 }
